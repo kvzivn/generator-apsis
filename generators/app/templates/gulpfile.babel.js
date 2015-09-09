fcbf3c5d8dac78b3a31b37c0303b2f38d523c367
Hampus Persson <h@hmps.se>
2015-09-09 16:32:05 +0200

    update gulpfile template

--- generators/app/templates/gulpfile.babel.js
+++ generators/app/templates/gulpfile.babel.js
@@ -1,43 +1,60 @@
 /* eslint-env node */
-import { watch, serve, stylus, clean, eslint, sequence } from 'strawpedo';
+import { bump, clean, copy, eslint, exec, sequence, serve, stylus, watch } from 'strawpedo';
 
-// If you need nib and ui-components, install nib and glob via npm and uncomment
+// If you need nib and Tenko, install nib and glob via npm and uncomment
 // these lines.
 
-// import nib from 'nib';
-// import glob from 'glob';
+// NIB AND TENKO
+import nib from 'nib';
+import glob from 'glob';
 
-// let stylusIncludePath = glob.sync(process.cwd() + '/jspm_packages/apsis/ui-components*/');
-// let stylusImportPaths = [
-//             'nib',
-//             stylusIncludePath + 'helpers/*.styl',
-//             stylusIncludePath + 'settings/*.styl'
-// ]
-//
-// stylusOptions = {
-    // use: nib(),
-    // include: stylusIncludePath,
-    // import: stylusImportPaths,
-    // compress: false
-// }
+const stylusIncludePath = glob.sync(process.cwd() + '/jspm_packages/apsis/tenko*/');
 
-sequence('default', [ 'clean', [ 'stylus', 'eslint' ], 'watch', 'serve' ]);
 
-// TASKS
+// SEQUENCE TASKS
+sequence('default', [ 'npm:install', 'clean', [ 'stylus', 'eslint' ], 'watch', 'serve' ]);
+sequence('release', [ 'clean', [ 'stylus:dist', 'eslint' ], 'copy:dist', 'bump' ]);
+
+
 watch({ name: 'watch', watchers: [
     { path: 'src/stylesheets/*.styl', tasks: ['stylus'] },
-    { path: 'src/**/*.js', tasks: ['eslint'] }
+    { path: 'src/**/*.js', tasks: ['eslint'] },
 ]});
 
+
+/**
+ * Compile stylus files.
+ *
+ * If you are using Nib and Tenko make sure to uncomment that section up top and
+ * the three option keys below.
+ */
 stylus({
     name: 'stylus',
     src: 'src/stylesheets/*.styl',
     dest: 'src/stylesheets',
     stylus: {
-        compress: false
-    }
+        use: nib(),
+        include: stylusIncludePath,
+        compress: false,
+    },
 });
 
+
+stylus({
+    name: 'stylus:dist',
+    src: 'src/stylesheets/*.styl',
+    dest: 'dist/stylesheets',
+    stylus: {
+        use: nib(),
+        include: stylusIncludePath,
+        compress: true,
+    },
+});
+
+
+/**
+ * Kick-off a server instance with the demo
+ */
 serve({
     port: 9000,
     startPath: 'demo/',
@@ -58,5 +75,33 @@ serve({
     }
 });
 
+
+/**
+ * Clean out the dist folder
+ */
 clean({ paths: 'dist/' });
+
+
+/**
+ * Lint the JS in the src directory
+ */
 eslint({ src: 'src/**/*.js' });
+
+
+/**
+ * Copy all JS and HTML files to the dist directory
+ */
+copy({
+    name: 'copy:dist',
+    src: [ 'src/**/*.js', 'src/**/*.html' ],
+    dest: 'dist/',
+});
+
+bump({});
+
+exec({
+    name: 'npm:install',
+    cmd: 'npm install',
+    message: 'Starting npm install. This takes a while, a full record of the operation will be printed to stdout once it completes.',
+    stdout: true,
+});
