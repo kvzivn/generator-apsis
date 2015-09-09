99e646a383cd3d849650b886a9f3be37b373ce53
Hampus Persson <h@hmps.se>
2015-09-09 16:35:47 +0200

    update demo file templates

--- generators/app/templates/_templates/_demo.js
+++ generators/app/templates/_templates/_demo.js
@@ -1,5 +1,5 @@
-import angular from 'angular';
-
 import { <%= ModuleName %> } from 'src/<%= moduleName %>.module';
 
 angular.module('demo', ['ng', <%= ModuleName %>.name]);
+angular.module('demo', ['ng']);
+angular.element(document).ready(() => angular.bootstrap(document, ['<%= ModuleName %>']));
