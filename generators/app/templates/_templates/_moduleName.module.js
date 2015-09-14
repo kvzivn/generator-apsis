348cc4b2aa1ca71e4a1bf9637a0ceb51e753e6d9
Hampus Persson <h@hmps.se>
2015-09-14 15:47:27 +0200

    fix case of module name in .module.js
    it was PascalCase, which is reserved for constructors. changed
    it to camelCase

--- generators/app/templates/_templates/_moduleName.module.js
+++ generators/app/templates/_templates/_moduleName.module.js
@@ -2,7 +2,7 @@ import { exampleDirective } from './javascript/example.directive';
 
 import './stylesheets/styles.css!';
 
-var <%= ModuleName %> = angular.module('Apsis.modules.<%= moduleName %>', ['ng'])
+var <%= moduleName %> = angular.module('Apsis.modules.<%= moduleName %>', ['ng'])
     .directive('aExampleDirective', exampleDirective);
 
-export { <%= ModuleName %> };
+export { <%= moduleName %> };
