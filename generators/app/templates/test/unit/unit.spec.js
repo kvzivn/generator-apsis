e59296cc8a2a8847aca1156f8fd72493e4ccb9c8
Hampus Persson <h@hmps.se>
2015-09-23 13:37:57 +0200

    update the unit spec template

--- generators/app/templates/test/unit/unit.spec.js
+++ generators/app/templates/test/unit/unit.spec.js
@@ -1,9 +1,5 @@
-import angular from 'angular';
-import exampleModule from 'src/example.module';
-import 'angular-mocks';
-
-describe('Example module', function() {
-
-    beforeEach(angular.mock.module(exampleModule.name));
-
+describe('Example module', () => {
+    it('should run', () => {
+        expect(true).toEqual(true);
+    });
 });
